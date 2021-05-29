de46f75a0623c461855da47df172b05923c1f362
Ryan Booth <ryanboothforever@gmail.com>
2021-05-29 08:00:01 -0500

    club collection template made, user profile upload image made but not connected

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+let loadFile = function (event) {
+  let output = document.getElementById("output");
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function () {
+    URL.revokeObjectURL(output.src);
+  };
+};
+let loadPost = function (e) {
+  let output = document.getElementById("output");
+};
