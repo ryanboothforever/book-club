6d113768f4fafdeb147094c929678a81a1f94f05
Ryan Booth <ryanboothforever@gmail.com>
2021-06-01 07:34:47 -0500

    clubc controller updated logic

--- controllers/clubs.js
+++ controllers/clubs.js
@@ -5,8 +5,28 @@ const Club = require('../models/Clubs');
 module.exports = {
   getClub: async (req, res) => {
     try {
-      const club = await Club.findById(req.params.id);
-      res.render('profile.ejs', { clubs: club, user: req.user });
+      const clubs = await Club.find().sort({ createdAt: 'desc' }).lean();
+      res.render('profile.ejs', { clubs: clubs, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createClub: async (req, res) => {
+    try {
+      // upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+      console.log(result);
+      await Club.create({
+        bookTitle: req.body.bookTitle,
+        bookAuthor: req.body.bookAuthor,
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+        synopsis: req.body.synopsis,
+        founder: req.user.id,
+        memebers: [],
+      });
+      console.log('Entry has been added!');
+      res.redirect('/profile');
     } catch (err) {
       console.log(err);
     }
